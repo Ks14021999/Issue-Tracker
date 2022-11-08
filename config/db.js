22e49022faf19628bdf9b32872c9a3d183f2f6e7
Ks14021999 <ks14021999@gmail.com>
2022-11-08 22:52:49 +0530

    2.0

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 main().catch(err => console.log("Database is not connected"));
 
 async function main(){
-    await mongoose.connect(process.env.URI);
+    await mongoose.connect('mongodb+srv://ks14:qazwsxedc@mern1.xq4bs.mongodb.net/test?retryWrites=true&w=majority');
     console.log('Database is connected');
 }
 
